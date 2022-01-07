7aa7a5f9812e7a0bc896a4b3ec7a1c4f60b63840
USER <USER@DESKTOP-EO932BE>
2022-01-07 21:57:43 +0900

    입력 값의 타입 변경 및 조건문 추가 Input type change(int->String) and add if

--- Jinjoo_PlusCycle/Program.cs
+++ Jinjoo_PlusCycle/Program.cs
@@ -8,12 +8,20 @@ namespace baekjoon
         {
             //값을 입력받는 부분
             Console.Write("0-99 사이의 정수를 입력하세요: ");
-            int input = int.Parse(Console.ReadLine());
+
+            //처음에 0으로 값을 줬으나 주어진 수가 10보다 작을 때 뒤에 0을 붙이기 위해 String 타입으로 받음
+            String input = Console.ReadLine();
 
             //처음 명시 된 "0보다 크거나 같고, 99보다 작거나 같은 수"에 해당하는지 판단하는 조건문
-            if (input >= 0 && input <= 99)
+            if (int.Parse(input) >= 0 && int.Parse(input) <= 99)
             {
-                Console.WriteLine("조건에 헤딩!");
+                
+                //입력 받은 값이 10보다 작으면 뒤에 0을 붙여서 10의 자리 수로 만들기
+                if (int.Parse(input) < 10)
+                {
+                    input = input + 0;
+                    Console.WriteLine(input);
+                }
             }
 
             else
